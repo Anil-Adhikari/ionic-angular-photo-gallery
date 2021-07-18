5c10d9a23aeae666ace567be54e1f51ef0a494aa
Adhikari <aadhika1@ford.com>
2021-07-18 14:21:43 -0400

    Sample project for learning Ionic for Angular Project

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,11 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoService: PhotoService) { }
+
+  async addPhotoToGallery() {
+    await this.photoService.loadSaved();
+    this.photoService.addNewToGallery();
+  }
 
 }
